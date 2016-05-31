82821a4a1a86dabf4a5c9cf24e28f10a77e9948a
dnhdang94 <dnhdang94@gmail.com>
2016-05-31 16:25:22 +0700

    630A

---
+++ 630A/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: May 31, 2016
+ *      Author: dnhdang94
+ */
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	float x;
+	scanf("%f", &x);
+	printf("25");
+	return 0;
+}
